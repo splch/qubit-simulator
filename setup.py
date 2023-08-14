ad95ae8d77051047549c4f985f2a431f1773ff6d
Spencer Churchill <spence@duck.com>
2023-08-14 03:51:06 -0700

    updates versions

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="qubit_simulator",
-    version="0.0.2",
+    version="0.0.3",
     description="A simple qubit simulator",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="Spencer Churchill",
     author_email="churchill@ionq.com",
     packages=find_packages(),
-    install_requires=["numpy>=1.10.1"],
+    install_requires=["numpy>=1.16.0"],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Science/Research",
