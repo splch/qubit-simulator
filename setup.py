e488aabf59327163a30843a9dfd75dc2baa567e5
Spencer Churchill <churchill@ionq.co>
2023-08-18 00:31:03 -0700

    use path fix

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
 from setuptools import setup, find_packages
+from os import path
+
+# Get the directory where this script is located
+here = path.abspath(path.dirname(__file__))
+
+# Read the requirements from the requirements.txt file
+with open(path.join(here, "requirements.txt")) as f:
+    requirements = f.readlines()
 
 setup(
     name="qubit_simulator",
     version="0.0.6",
     description="A simple qubit simulator",
-    long_description=open("README.md").read(),
+    long_description=open(path.join(here, "README.md")).read(),
     long_description_content_type="text/markdown",
     author="Spencer Churchill",
     author_email="churchill@ionq.com",
     packages=find_packages(),
-    install_requires=open("requirements.txt").readlines(),
+    install_requires=requirements,
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Science/Research",
